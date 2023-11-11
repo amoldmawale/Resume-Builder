8edebc94d2c3a73536e069e35921d3f15412292a
amoldmawale <143996360+amoldmawale@users.noreply.github.com>
2023-11-11 23:15:40 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,113 @@
+function addNewWEField()
+{
+    // console.log("Adding new field")
+   let newNode = document.createElement("textarea");
+   newNode.classList.add("form-control");
+   newNode.classList.add("weField");
+   newNode.classList.add("mt-2")
+   newNode.setAttribute("rows",3);
+   newNode.setAttribute("placeholder","Enter Here");
+
+
+    let weOb = document.getElementById("we");
+
+   let weAddButtonOb= document.getElementById("weAddButton");
+
+   weOb.insertBefore(newNode,weAddButtonOb);
+
+}
+
+function addNewAQField()
+{
+      // console.log("Adding new field")
+   let newNode = document.createElement("textarea");
+   newNode.classList.add("form-control");
+   newNode.classList.add("eqField");
+   newNode.classList.add("mt-2")
+   newNode.setAttribute("rows",3);
+   newNode.setAttribute("placeholder","Enter Here");
+
+
+    let aqOb = document.getElementById("aq");
+
+   let aqAddButtonOb= document.getElementById("aqAddButton");     
+
+   aqOb.insertBefore(newNode,aqAddButtonOb);
+}
+
+
+//Generating CV
+
+function generateCV()
+{
+   let nameField = document.getElementById("nameField").value;
+
+   let nameT1=document.getElementById("nameT1");
+
+   nameT1.innerHTML = nameField;
+
+   document.getElementById("nameT2").innerHTML = nameField;
+
+   //For contact
+   document.getElementById("contactT").innerHTML= document.getElementById("contactField").value;
+
+   //For address
+   document.getElementById("addressT").innerHTML=document.getElementById("addressField").value;
+
+   //for Facebook Link
+   document.getElementById("fbT").innerHTML= document.getElementById("fbField").value;
+
+   //for Instagram
+   document.getElementById("instaT").innerHTML= document.getElementById("instaField").value;
+   
+   //For LinkedIn
+   document.getElementById("linkedT").innerHTML=document.getElementById("linkedField").value;
+
+   //for Objectives
+   document.getElementById("objectiveT").innerHTML=document.getElementById("objectiveField").value;
+
+   //for work Experience
+  let wes= document.getElementsByClassName ("weField");
+  let str="";
+  for (let e of wes) {
+   str = str + `<li>${e.value}</li>`;
+  }
+  document.getElementById("weT").innerHTML=str;
+
+  //for academic qualication
+
+let aqs=document.getElementsByClassName("eqField");
+let str1 ="";
+for (let e of aqs)
+{
+   str1 +=`<li>${e.value}</li>`;
+document.getElementById("aqT").innerHTML=str1;
+}
+
+//For seeting image
+
+let file=document.getElementById("imgField").files[0]
+console.log(file);
+
+let reader=new FileReader()
+reader.readAsDataURL(file);
+console.log(reader.result);
+//Set the image to template
+
+reader.onloadend=function(){
+
+document.getElementById("imgT").src=reader.result;
+}
+
+document.getElementById("cv-form").style.display="none";
+document.getElementById("cv-template").style.display="block";
+}
+
+// For print CV
+function printCV()
+{
+   window.print();
+}
+
+
+
